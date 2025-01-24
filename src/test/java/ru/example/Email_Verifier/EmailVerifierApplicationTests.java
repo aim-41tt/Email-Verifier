a1d0e358eedd53cb09c5665c08d5ea49f4a29c15
aim_41tt <0505mustik@gmail.com>
2025-01-24 13:30:34 +0300

    added test

---
+++ src/test/java/ru/example/Email_Verifier/EmailVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.Email_Verifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
